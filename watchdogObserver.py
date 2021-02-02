e81cd75c048ad446826674cdfe74f49a0e42aad8
Larry-B4 <larissa.bosshard@edu.tbz.ch>
2021-02-02 12:56:59 +0100

    Added comments for variables

--- watchdogObserver.py
+++ watchdogObserver.py
@@ -3,14 +3,14 @@ from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 if __name__ == "__main__":
-    patterns = "*"
-    ignore_patterns = ""
+    patterns = "*" # Patterns that we want to handle
+    ignore_patterns = "" # Patterns that will be ignored
     ignore_directories = False
-    case_sensitive = True
+    case_sensitive = True # Patterns are treated case-sensitive
     my_event_handler = PatternMatchingEventHandler(
         patterns, ignore_patterns, ignore_directories, case_sensitive)
 
-
+# Methods to be called when a specific event is raised
 def on_created(event):
     print(f"hey, {event.src_path} has been created!")
 
@@ -26,14 +26,14 @@ def on_modified(event):
 def on_moved(event):
     print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
 
-
+# Define which method is called on which event
 my_event_handler.on_created = on_created
 my_event_handler.on_deleted = on_deleted
 my_event_handler.on_modified = on_modified
 my_event_handler.on_moved = on_moved
 
-path = "."
-go_recursively = True
+path = "." # Monitored Path
+go_recursively = True # Subdirectories are monitored as well
 my_observer = Observer()
 my_observer.schedule(my_event_handler, path, recursive=go_recursively)
 
