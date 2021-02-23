2251740b4a7c8c77e7c71c3ed324d0b37845778c
Larry-B4 <larissa.bosshard@edu.tbz.ch>
2021-02-23 10:07:51 +0100

    Add new function to create the output folder

--- watchdogObserver.py
+++ watchdogObserver.py
@@ -4,16 +4,25 @@ from watchdog.events import PatternMatchingEventHandler
 import os
 
 if __name__ == "__main__":
-    patterns = "*" # Patterns that we want to handle
-    ignore_patterns = "" # Patterns that will be ignored
+    patterns = "*"  # Patterns that we want to handle
+    ignore_patterns = ""  # Patterns that will be ignored
     ignore_directories = False
-    case_sensitive = True # Patterns are treated case-sensitive
+    case_sensitive = True  # Patterns are treated case-sensitive
     my_event_handler = PatternMatchingEventHandler(
         patterns, ignore_patterns, ignore_directories, case_sensitive)
 
 # Methods to be called when a specific event is raised
+
+
+def make_folder():
+    newpath = ".\Output"
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+
+
 def on_created(event):
     print(f"hey, {event.src_path} has been created!")
+    make_folder()
 
 
 def on_deleted(event):
@@ -27,14 +36,15 @@ def on_modified(event):
 def on_moved(event):
     print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
 
+
 # Define which method is called on which event
 my_event_handler.on_created = on_created
 my_event_handler.on_deleted = on_deleted
 my_event_handler.on_modified = on_modified
 my_event_handler.on_moved = on_moved
 
-path = "." # Monitored Path
-go_recursively = True # Subdirectories are monitored as well
+path = "."  # Monitored Path
+go_recursively = True  # Subdirectories are monitored as well
 my_observer = Observer()
 my_observer.schedule(my_event_handler, path, recursive=go_recursively)
 
